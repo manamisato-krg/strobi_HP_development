597e931da7a6db6886055621d9315d32059346c4
Manami <manami.sato@dixusjp.com>
2026-03-20 11:17:53 +0900

    デバック

--- news.js
+++ news.js
@@ -14,20 +14,22 @@ const newsData = [
         summary: "くらげクラフト発売のリリースを、VOIX Bizにて取り上げていただきました。",
         url: "news/news-03.html",
         category: "notice"
-
+    },
+    {
         date: "2026.03.31",
         tag: "お知らせ",
         title: "くらげクラフト発売のリリースが、サステナビリティ情報を紹介するWEBメディア cokiに掲載されました",
         summary: "くらげクラフト発売のリリースを、cokiにて取り上げていただきました。",
         url: "news/news-02.html",
         category: "notice"
-
+    },
+    {
         date: "2026.03.31",
         tag: "プレスリリース",
         title: "クラゲを使ったクラフトビール、くらげクラフトを発売しました",
         summary: "クラゲを使ったクラフトビール、くらげクラフトを発売しました。",
         url: "news/news-01.html",
         category: "press"
-    },
+    }
     // 以下、新しいニュースを上に追加していく
 ];
