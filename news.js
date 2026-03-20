f1e4e2e5e73ad092fdd55a0f88249d3ff8218f9d
Manami <manami.sato@dixusjp.com>
2026-03-20 11:14:09 +0900

    cokiを追加

--- news.js
+++ news.js
@@ -14,6 +14,13 @@ const newsData = [
         summary: "くらげクラフト発売のリリースを、VOIX Bizにて取り上げていただきました。",
         url: "news/news-03.html",
         category: "notice"
+
+        date: "2026.03.31",
+        tag: "お知らせ",
+        title: "くらげクラフト発売のリリースが、サステナビリティ情報を紹介するWEBメディア cokiに掲載されました",
+        summary: "くらげクラフト発売のリリースを、cokiにて取り上げていただきました。",
+        url: "news/news-02.html",
+        category: "notice"
     },
     // 以下、新しいニュースを上に追加していく
 ];
