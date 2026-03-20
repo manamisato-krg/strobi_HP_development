61d92d6046051401155fa19374decd359737bda3
Manami <manami.sato@dixusjp.com>
2026-03-20 11:10:12 +0900

    ニュースのJavaScriptを作成

---
+++ news.js
@@ -0,0 +1,19 @@
+const newsData = [
+    {
+        date: "2026.03.31",
+        tag: "お知らせ",
+        title: "社名変更および本店移転のお知らせ",
+        summary: "この度、strobi Inc.は社名変更および本店移転をいたしました。",
+        url: "news/news-04.html",
+        category: "notice"
+    },
+    {
+        date: "2026.03.31",
+        tag: "プレスリリース",
+        title: "クラゲを使ったクラフトビール、くらげクラフトを発売しました",
+        summary: "2月19日(木)に、クラゲを実際に使用したクラフトビール、「くらげクラフト」を発売いたしました。",
+        url: "news/news-01.html",
+        category: "press"
+    },
+    // 以下、新しいニュースを上に追加していく
+];
