f2e4ea3d2ee17a82a40175aa9f1ca0b7ffae02e5
Manami <manami.sato@dixusjp.com>
2026-03-20 11:25:57 +0900

    Jsに画像を追加

--- news.js
+++ news.js
@@ -3,33 +3,36 @@ const newsData = [
         date: "2026.03.31",
         tag: "お知らせ",
         title: "社名変更および本店移転のお知らせ",
-        summary: "社名変更および本店移転をいたしました。",
+        summary: "この度、strobi Inc.は社名変更および本店移転をいたしました。",
         url: "news/news-04.html",
-        category: "notice"
+        category: "notice",
+        image: ""  // 画像なしの場合は空のまま
     },
     {
         date: "2026.03.31",
         tag: "お知らせ",
         title: "くらげクラフト発売のリリースが、「VOIX Biz」に掲載されました",
-        summary: "くらげクラフト発売のリリースを、VOIX Bizにて取り上げていただきました。",
+        summary: "株式会社VOIXが運営する、ビジネスの最新情報をお届けするWebメディア「VOIX Biz」にくらげクラフトが掲載されました。",
         url: "news/news-03.html",
-        category: "notice"
+        category: "notice",
+        image: "images_hp/VOIX-biz-logo-small.png"
     },
     {
         date: "2026.03.31",
         tag: "お知らせ",
         title: "くらげクラフト発売のリリースが、サステナビリティ情報を紹介するWEBメディア cokiに掲載されました",
-        summary: "くらげクラフト発売のリリースを、cokiにて取り上げていただきました。",
+        summary: "株式会社Saccoが運営する、法人のサステナビリティ情報を紹介するWEBメディアサイト「coki」に掲載されました。",
         url: "news/news-02.html",
-        category: "notice"
+        category: "notice",
+        image: "images_hp/coki_new.jpg"
     },
     {
         date: "2026.03.31",
         tag: "プレスリリース",
         title: "クラゲを使ったクラフトビール、くらげクラフトを発売しました",
-        summary: "クラゲを使ったクラフトビール、くらげクラフトを発売しました。",
+        summary: "2月19日(木)に、クラゲを実際に使用したクラフトビール、「くらげクラフト」を発売いたしました。限定400本を、クラウドファンディングサイトにて販売中です。",
         url: "news/news-01.html",
-        category: "press"
-    }
-    // 以下、新しいニュースを上に追加していく
+        category: "press",
+        image: "images_hp/ser_kurage craft.jpg"
+    },
 ];
